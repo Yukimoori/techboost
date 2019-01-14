ed6d7721470181840a541cd5e2a784987b1a7abd
”mori_mac” <ihrnogqb@yahoo.co.jp>
2019-01-14 23:15:16 +0900

    mac update

--- jQuery03_kadai.js
+++ jQuery03_kadai.js
@@ -9,24 +9,53 @@
 //   });
 // })(jQuery, window);
 
-// 課題２
+// 課題２　未完成
+// (function($, window) {
+//   $(function() {
+//     $('#login').on('click', function(e) {
+//       var email = $('#email').val(),
+//           password = $('#password').val();
+//
+//       if (email == '') {
+//         alert('メールアドレスを入力してください');
+//       } else if (email.indexOf('@') < 0) {
+//         alert('メールアドレスには必ず「@」が必要です');
+//       }
+//       if (password == '') {
+//         alert('パスワードを入力してください');
+//       } else if (password.length < 6) {
+//         alert('パスワードは6文字以上です');
+//       }
+//     });
+//   });
+// })(jQuery, window);
+
+
+// 課題３　未完成
+
+
+// 課題４
+// (function($, window) {
+//   $(function() {
+//     $('#date_picker').flatpickr();
+//   });
+// })(jQuery, window);
+
+// 課題５完成→６ 未完成
 (function($, window) {
   $(function() {
-    $('#login').on('click', function(e) {
-      var email = $('#email').val(),
-          password = $('#password').val();
-      // alert(email);
-      // alert(password);
-      if (email == '') {
-        alert('メールアドレスを入力してください');
-      } else if (email.indexOf('@') < 0) {
-        alert('メールアドレスには必ず「@」が必要です');
-      }
-      if (password == '') {
-        alert('パスワードを入力してください');
-      } else if (password.length < 6) {
-        alert('パスワードは6文字以上です');
-      }
-    });
+    // ここに処理を書きます
+  if($('#kensaku').on('click', function()) {
+    $('#zip1').jpostal({
+        postcode : [
+            '#zip'
+        ],
+        address : {
+            '#pref'  : '%3',
+            '#city'  : '%4',
+            '#street'  : '%5'
+        }
+
+    })};
   });
 })(jQuery, window);
